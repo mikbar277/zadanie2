451f300986e6978c530a849932233b7a9c8a84be
mikbar277 <s25348@pjwstk.edu.pl>
2024-04-20 23:37:56 +0200

    Added default sorting for endpoint used to return list of animals

---
+++ WebApplicationHorizontal/Controllers/AnimalsController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Services;
+
+namespace WebApplication1.Controllers;
+
+public class AnimalController : ControllerBase
+{
+    private IAnimalsService _animalService;
+    
+    public Animals
+}
